45a3a83e786147108e87c9177e5e57d84bf956d5
manideeppasuladi <44994267+manideeppasuladi@users.noreply.github.com>
2019-04-21 11:49:29 +0530

    css and js

--- js/javascript/load.js
+++ js/javascript/load.js
@@ -1,6 +1,5 @@
-$(window).load(function() {
-		// Animate loader off screen
-        $(".se-pre-con").fadeOut("slow");;
-        sleep(10);
-        exit();
-	});
+var loading = document.getElementById('loading');
+
+window.addEventListener('load',function() {
+    loading.style.display =  'none';
+})
